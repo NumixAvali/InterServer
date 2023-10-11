79365ab0ab96f5912e433335979a66dc9c3a5327
NumixAvali <61939112+NumixAvali@users.noreply.github.com>
2023-10-11 13:19:30 +0300

    Move JS code to corresponding file and make button nicer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,16 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function refreshBtnClick() {
+    alert('Not implemented yet.');
+    // $.ajax({
+    //     type: "POST",
+    //     url: "/ControllerName/ActionName",
+    //     success: function (data) {
+    //         // Handle the response from the server, e.g., update the UI.
+    //     },
+    //     error: function (error) {
+    //         // Handle errors, if any.
+    //     }
+    // });
+}
