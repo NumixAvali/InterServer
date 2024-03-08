68d7fbff1fb04c1feae9d4eeaab3df282a8adeed
NumixAvali <61939112+NumixAvali@users.noreply.github.com>
2024-03-08 18:20:48 +0200

    More progress on the history page

--- wwwroot/js/charting.js
+++ wwwroot/js/charting.js
@@ -1,54 +1,52 @@
 let ctx = document.getElementById('historicChart').getContext('2d');
 
+function timeConverter(unixTimestamp){
+	let a = new Date(unixTimestamp * 1000);
+	let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	let year = a.getFullYear();
+	let month = months[a.getMonth()];
+	let date = a.getDate();
+	let hour = a.getHours();
+	let min = a.getMinutes();
+	let sec = a.getSeconds();
+	let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+	return time;
+}
+
 function getData() {
-	let data = [];
+	let data = {
+		batterySoc: {
+			labels: [],
+			values: []
+		}
+	}
 	
 	try {
 		$.ajax({
 			type: "POST",
-			url: "/api/v1/historic-cache",
+			url: "/api/v1/cache/timestamp-range",
 			async: false,
 			contentType: "application/json; charset=utf-8",
 			dataType: "json",
 			data: JSON.stringify({
-				timestamp: 1709216936,
+				timestampStart: 1709216936,
+				timestampEnd: 1709319936,
 				token: "a"
 			}),
-			success: function(response) {
+			success: function (response) {
 				console.log(response);
-				data.push(response.data.data.loadVoltage.value * response.data.data.loadVoltage.scale)
+				
+				response.data.forEach((entry) => {
+					data.batterySoc.labels.push(timeConverter(entry.timestamp))
+					data.batterySoc.values.push(entry.data.batterySoc.value * entry.data.batterySoc.scale)
+				})
+				// data.push(response.data.data.loadVoltage.value * response.data.data.loadVoltage.scale)
 			},
-			error: function(error) {
+			error: function (error) {
 				console.error("Error retrieving cache:", error);
 				// Handle error appropriately
 			}
-		});
-		$.ajax({
-			type: "GET",
-			url: "/api/v1/latest-cache",
-			async: false,
-			success: function (reply) {
-				// console.log(reply)
-				data.push(reply.data.data.loadVoltage.value * reply.data.data.loadVoltage.scale);
-			},
-			error: function (error) {
-				console.error(`Refresh data (2) error!\n`, error);
-				// Handle error appropriately
-			}
-		});
-		$.ajax({
-			type: "GET",
-			url: "/api/v1/current-data",
-			async: false,
-			success: function (reply) {
-				// console.log(reply)
-				data.push(reply.data.loadVoltage.value * reply.data.loadVoltage.scale);
-			},
-			error: function (error) {
-				console.error(`Refresh data (1) error!\n`, error);
-				// Handle error appropriately
-			}
-		});
+		})
 	} catch (error) {
 		console.error("Error during synchronous AJAX calls:", error);
 		// Handle errors gracefully
@@ -58,17 +56,17 @@ function getData() {
 }
 
 
-console.log(getData())
+// console.log(getData())
 
 // Define your data
-var data = {
-	labels: ['Historic','Past', 'Present'],
+let data = {
+	labels: getData().batterySoc.labels,
 	datasets: [{
 		label: 'Battery level %',
-		data: getData(),
+		data: getData().batterySoc.values,
 		backgroundColor: [
 			'rgba(255, 99, 132, 0.2)',
-			'rgba(54, 162, 235, 0.2)',
+			// 'rgba(54, 162, 235, 0.2)',
 			// 'rgba(255, 206, 86, 0.2)',
 			// 'rgba(75, 192, 192, 0.2)',
 			// 'rgba(153, 102, 255, 0.2)',
@@ -76,7 +74,7 @@ var data = {
 		],
 		borderColor: [
 			'rgba(255, 99, 132, 1)',
-			'rgba(54, 162, 235, 1)',
+			// 'rgba(54, 162, 235, 1)',
 			// 'rgba(255, 206, 86, 1)',
 			// 'rgba(75, 192, 192, 1)',
 			// 'rgba(153, 102, 255, 1)',
@@ -87,7 +85,7 @@ var data = {
 };
 
 // Create the chart
-var myChart = new Chart(ctx, {
+let myChart = new Chart(ctx, {
 	type: 'line',
 	data: data,
 	options: {
