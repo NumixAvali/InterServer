4b61055231ab4bb886477f49a49c06e4134e3eb0
NumixAvali <61939112+NumixAvali@users.noreply.github.com>
2024-03-08 15:21:29 +0200

    Finalize DataCollector.cs

--- Logic/DataCollector.cs
+++ Logic/DataCollector.cs
@@ -13,34 +13,32 @@ public class DataCollector : BackgroundService
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        AppSettings settings = new SettingsController().GetSettings();
-
-        if (!settings.EnableAutomaticDataGather) return;
-            
         while (!stoppingToken.IsCancellationRequested)
         {
-            // TODO: Implement DB interaction
-            // Console.WriteLine("Worker running at: {0}", DateTimeOffset.Now);
-
             RequestHandler requestHandler = new RequestHandler();
             AppSettings appSettings = new SettingsController().GetSettings();
             
             bool isConfigSafe = appSettings.GetType().GetProperties()
                 .All(p => p.GetValue(appSettings) != null);
             
-            if (!isConfigSafe) return;
-
-            DbHandler dbHandler = new DbHandler(
-                appSettings.DbIp,
-                appSettings.DbName,
-                appSettings.DbUsername,
-                appSettings.DbPassword
+            if (appSettings.EnableAutomaticDataGather && isConfigSafe)
+            {
+                DbHandler dbHandler = new DbHandler(
+                    appSettings.DbIp,
+                    appSettings.DbName,
+                    appSettings.DbUsername,
+                    appSettings.DbPassword
                 );
-            
-            dbHandler.UploadData(requestHandler.ResponseManager(ResponseType.Ok,ReplyDataType.CurrentData));
 
-            // TODO: finish making setting menu, so this horror can be avoided
-            await Task.Delay(10*60*1000, stoppingToken);
+                dbHandler.UploadData(requestHandler.ResponseManager(ResponseType.Ok, ReplyDataType.CurrentData));
+
+                await Task.Delay(Convert.ToInt32(appSettings.AutomaticGatherInterval) * Convert.ToInt32(appSettings.AutomaticGatherIntervalModifier), stoppingToken);
+            }
+            else
+            {
+                // Console.WriteLine("Data gathering disabled");
+                Thread.Sleep(1000);
+            }
         }
     }
 
