827c2e7a3d572a675b5356e586f8c106c8d445ff
NumixAvali <61939112+NumixAvali@users.noreply.github.com>
2024-03-19 00:36:34 +0200

    Preparation for working under the non-root condition of the web server

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{prefix?}/{controller=Home}/{action=Index}/{id?}");
 
 app.UseRateLimiter();
 
