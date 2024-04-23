4238b7f23387fd9e4ac2e9479746f1599303c163
NumixAvali <61939112+NumixAvali@users.noreply.github.com>
2024-04-23 12:52:21 +0300

    Fix and expand settings backup logic

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -39,30 +39,48 @@ public class SettingsController : ControllerBase
             WriteIndented = true
         });
 
+        string[] backupLevels = { ".bak", ".bak1", ".bak2", ".bak3" };
+
         if (System.IO.File.Exists("./settings.json"))
         {
             try
             {
-                System.IO.File.Move("./settings.json","./settings.json.bak");
+                for (int i = backupLevels.Length - 1; i > 0; i--)
+                {
+                    string currentBackupFile = $"./settings.json{backupLevels[i]}";
+                    string previousBackupFile = $"./settings.json{backupLevels[i - 1]}";
+
+                    if (System.IO.File.Exists(currentBackupFile))
+                    {
+                        System.IO.File.Delete(currentBackupFile);
+                    }
+
+                    if (System.IO.File.Exists(previousBackupFile))
+                    {
+                        System.IO.File.Move(previousBackupFile, currentBackupFile);
+                    }
+                }
+
+                System.IO.File.Move("./settings.json", $"./settings.json{backupLevels[0]}");
             }
             catch (Exception e)
             {
-                Console.WriteLine("[Settings] Error backing up settings file:\n"+e);
+                Console.WriteLine("[Settings] Error backing up settings file:\n" + e);
                 // throw;
             }
         }
 
         try
         {
-            System.IO.File.WriteAllText("./settings.json",jsonSettings);
+            System.IO.File.WriteAllText("./settings.json", jsonSettings);
         }
         catch (Exception e)
         {
-            Console.WriteLine("[Settings] Error writing settings file:\n"+e);
+            Console.WriteLine("[Settings] Error writing settings file:\n" + e);
             throw;
         }
     }
-
+    
     public string[] GetConfigs()
     {
         try
